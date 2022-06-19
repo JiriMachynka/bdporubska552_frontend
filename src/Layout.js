5d2c80217c65f1f7659e9be6c6cc7931396ddffd
MachyyDev <jiri.machynka@mendelova-stredni.cz>
2022-06-19 17:17:23 +0200

    Navbar moved

--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,4 @@
-//import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
-import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 
@@ -8,7 +6,6 @@ function Layout() {
     return (
         <div className="container mx-auto">
             <Header />
-            <Navbar />
             <Main />
             <Footer />
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import logo from '../images/logo-vezicky-color.png'   
+import Navbar from './Navbar'
 
 function Header(){
     return (
         <header className="container bg-zinc-400">
             <img className="float-left" src={logo} alt="Logo bytového družstva" />
             <h1 className="text-center text-3xl font-bold">Bytové družstvo Porubská 552</h1>
-            <h3>Informační portál bytového družstva</h3>
+            <h3 className="text-center">Informační portál bytového družstva</h3>
+            <Navbar />
         </header>
     )
 }

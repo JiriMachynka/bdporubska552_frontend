502336f177f12ab63b86837e00e43f078504dc4a
MachyyDev <jiri.machynka@mendelova-stredni.cz>
2022-06-19 17:17:16 +0200

    Styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 function Main() {
     return(
-        <article className='container bg-gray-400'>
+        <article className='container bg-gray-400 clear-both px-2 py-3'>
             <Outlet />
         </article>
     )

6c3aff56a6352357a806e34cceef38008740bbc3
MachyyDev <jiri.machynka@mendelova-stredni.cz>
2022-06-27 15:37:45 +0200

    Edit and delete functions

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
 const Announcement = (props) => {
-    const { title, content, editAnnouncement } = props
+    const { _id, title, content, createdAt, editAnnouncement, deleteAnnouncement } = props
     return (
         <div className='flex flex-row bg-amber-300 rounded-xl p-2 gap-2'>
             <div className='flex flex-col gap-2'>
                 <button 
                     className='admin-button bg-blue-500 border-blue-500 hover:border-blue-800'
-                    onClick={() => editAnnouncement(title, content)}
+                    onClick={() => editAnnouncement(_id, title, content)}
                 >
                     Upravit
                 </button>
                 <button 
                     className='admin-button bg-red-500 border-red-500 hover:border-red-800'
+                    onClick={() => deleteAnnouncement(_id)}
                 >
                     Smazat
                 </button>
@@ -20,6 +21,7 @@ const Announcement = (props) => {
             <div>
                 <div>{title}</div>
                 <div>{content}</div>
+                <div>Vytvořeno: {createdAt}</div>
             </div>
         </div>
     )
--- src/pages/ManageAnnouncements.js
+++ src/pages/ManageAnnouncements.js
@@ -5,43 +5,65 @@ import Announcement from '../components/Announcement'
 
 const ManageAnnouncements = () => {
     const [announcements, setAnnouncements] = useState([])
+    const [announcement, setAnnouncement] = useState({})
+    const [reload, setReload] = useState(false)
 
     useEffect(() => {
         axios.get('http://localhost:3001/api/announcement')
             .then((res) => setAnnouncements(res.data))
-    }, [])
+        setReload(false)
+    }, [reload])
+    
+    const editAnnouncement = (id) => {
+        setAnnouncement(...announcements.filter(announcement => announcement._id === id))
+    }
 
-    const editAnnouncement = (title, content) => {
-        console.clear()
-        console.log(`Title: ${title}`)
-        console.log(`Content: ${content}`)
+    const deleteAnnouncement = (id) => {
+        axios.delete(`http://localhost:3001/api/announcement/${id}`)
+        setReload(true)
     }
 
     const formik = useFormik({
+        enableReinitialize: true,
         initialValues: {
-            title: "",
-            content: "",
+            title: announcement.title || "",
+            content:  announcement.content || "",
         },
-        onSubmit: async (values) => {
-            console.log(values)
+        onSubmit: async (values, { resetForm }) => {
             const { title, content } = formik.values
-            axios.post('http://localhost:3001/api/announcement', {
-                title: title,
-                content: content,
-            }).then((res) => console.log(res))
-        }
+            if (announcement.title) {
+                axios.patch(`http://localhost:3001/api/announcement/${announcement._id}`, {
+                    title: title,
+                    content: content,
+                })
+                setAnnouncement({})
+            } else {
+                console.log(formik.values)
+                axios.post('http://localhost:3001/api/announcement', {
+                    title: title,
+                    content: content,
+                })
+            }
+            resetForm({ values: '' })
+            setReload(true)
+        },
     })
 
     return (
         <div className='grid grid-cols-2 gap-3'>
             <div className='flex flex-col gap-3'>
                 {announcements.map((announcement, index) => 
-                    <Announcement key={index} {...announcement} editAnnouncement={editAnnouncement} />
+                    <Announcement 
+                        key={index} 
+                        editAnnouncement={editAnnouncement} 
+                        deleteAnnouncement={deleteAnnouncement} 
+                        {...announcement} 
+                    />
                 )}
             </div>
             <div>
                 <form className='flex flex-col gap-2 mx-auto' onSubmit={formik.handleSubmit}>
-                    <label className='label' htmlFor='title'>Title</label>
+                    <label className='label' htmlFor='title'>Nadpis</label>
                     <input
                         id="title"
                         name="title"
@@ -50,7 +72,7 @@ const ManageAnnouncements = () => {
                         onChange={formik.handleChange}
                         value={formik.values.title}
                     />
-                    <label className='label' htmlFor='content'>Content</label>
+                    <label className='label' htmlFor='content'>Obsah</label>
                     <textarea
                         id="content"
                         name="content"
@@ -67,7 +89,7 @@ const ManageAnnouncements = () => {
                         className='border-[3px] border-green-300 bg-green-300 rounded-lg font-bold h-[50px] text-2xl
                                 hover:border-black'
                     >
-                        Odeslat
+                        {announcement.title ? "Aktualizovat oznámení" : "Nové oznámení"}
                     </button>
                 </form>
             </div>

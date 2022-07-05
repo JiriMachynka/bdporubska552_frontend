94fd919f22cd5f30cbfd9949f448f81e34229633
MachyyDev <jiri.machynka@mendelova-stredni.cz>
2022-07-05 22:10:41 +0200

    Styling changes to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import Navbar from './Navbar'
 
 function Header(){
     return (
-        <header className="flex  md:flex-row sm:flex-col h-auto">
-            <img className="w-auto" src={logo} alt="Logo bytového družstva" />
-            <div className='w-full relative'>
+        <header className="flex flex-col md:flex-row">
+            <img className="w-[265px]" src={logo} alt="Logo bytového družstva" />
+            <div className='flex flex-col w-full relative'>
                 <h1 className="text-center text-4xl font-bold pt-[40px] ">Bytové družstvo Porubská 552</h1>
                 <h3 className="text-center text-2xl pt-[20px]">Informační portál bytového družstva</h3>
                 <Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-    <nav className='h-11 absolute bottom-0 w-full bg-sand-navbar'>
+    <nav className='h-11 mt-auto w-full bg-sand-navbar'>
         <ul className='flex flex-row h-full items-center justify-center gap-[15px]'>
             <li className='nav-item'>
                 <Link to='/'>Domů</Link>
             </li>
             <li className='nav-item'>
                 <Link to='/about'>O nás</Link>
-                <ul>
+                {/* <ul>
                     <li><Link to={'about/sprava'}>Správa domu</Link></li>
                     <li><Link to={'about/predstavenstvo'}>Představenstvo</Link></li>
                     <li className='nav-item'><Link to='about/history'>Historie</Link></li>
-                </ul>
+                </ul> */}
             </li>
             <li className='nav-item'>
                 <Link to='/documents'>Dokumenty</Link>

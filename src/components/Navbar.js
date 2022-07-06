1ded849e3d6cf93f9d54e655e5d49d9eeddd963b
MachyyDev <jiri.machynka@mendelova-stredni.cz>
2022-07-06 11:06:59 +0200

    Minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import MenuIcon from '@mui/icons-material/Menu'
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from '@mui/icons-material/Close'
 
 function Navbar() {
     const [opened, setOpened] = useState(false)
-
-    const toggleMenu = () => {
-        setOpened(!opened)
-    }
 
     return (
     <nav className='main-navbar h-full mt-auto w-full bg-sand-navbar md:h-11'>
@@ -33,7 +29,7 @@ function Navbar() {
             <li className='nav-item'>
                 <Link to='/contact'>Kontakt</Link>
             </li>
-            <div onClick={toggleMenu}>
+            <div onClick={() => setOpened(prev => !prev)}>
                 {opened ?
                     <CloseIcon className='hamburger-menu' style={{
                         marginRight: "12px",
@@ -48,28 +44,28 @@ function Navbar() {
                     }} />
                 }
             </div>
-        </ul>    
-        {opened &&
-            <ul className='dropdown'>
-                <li className='nav-item-dropped'>
-                    <Link to='/about'>O nás</Link>
-                    {/* <ul>
-                        <li><Link to={'about/sprava'}>Správa domu</Link></li>
-                        <li><Link to={'about/predstavenstvo'}>Představenstvo</Link></li>
-                        <li className='nav-item'><Link to='about/history'>Historie</Link></li>
-                    </ul> */}
-                </li>
-                <li className='nav-item-dropped'>
-                    <Link to='/documents'>Dokumenty</Link>
-                </li>
-                <li className='nav-item-dropped'>
-                    <Link to='/fotogalery'>Fotogalerie</Link>
-                </li>
-                <li className='nav-item-dropped'>
-                    <Link to='/contact'>Kontakt</Link>
-                </li>
-            </ul>
-        }
+        </ul>  
+            {opened &&
+                <ul className='dropdown'>
+                    <li className='nav-item-dropped'>
+                        <Link to='/about'>O nás</Link>
+                        {/* <ul>
+                            <li><Link to={'about/sprava'}>Správa domu</Link></li>
+                            <li><Link to={'about/predstavenstvo'}>Představenstvo</Link></li>
+                            <li className='nav-item'><Link to='about/history'>Historie</Link></li>
+                        </ul> */}
+                    </li>
+                    <li className='nav-item-dropped'>
+                        <Link to='/documents'>Dokumenty</Link>
+                    </li>
+                    <li className='nav-item-dropped'>
+                        <Link to='/fotogalery'>Fotogalerie</Link>
+                    </li>
+                    <li className='nav-item-dropped'>
+                        <Link to='/contact'>Kontakt</Link>
+                    </li>
+                </ul>
+            }
     </nav>
     )    
 }

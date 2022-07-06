28368561b1bd0389f9d4b1774c34db02653837a6
MartinStudnicka <koumes3@seznam.cz>
2022-07-06 21:05:33 +0200

    style img for md breakpoints

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Navbar from './Navbar'
 function Header(){
     return (
         <header className="flex flex-col md:flex-row">
-            <img className="w-[265px] mx-auto" src={logo} alt="Logo bytového družstva" />
+            <img className="w-full md:w-[265px] mx-auto" src={logo} alt="Logo bytového družstva" />
             <div className='flex flex-col w-full relative gap-3 md:gap-[20px]'>
                 <h1 className="text-center text-4xl font-bold md:mt-4">Bytové družstvo Porubská 552</h1>
                 <h3 className="text-center text-2xl">Informační portál bytového družstva</h3>

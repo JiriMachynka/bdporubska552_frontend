cc40d16642ff1f644c3a518cc67112ba4a42d14b
MachyyDev <jiri.machynka@mendelova-stredni.cz>
2022-07-06 10:45:52 +0200

    Added hamburger menu
    Animation still inwork

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import Navbar from './Navbar'
 function Header(){
     return (
         <header className="flex flex-col md:flex-row">
-            <img className="w-[265px]" src={logo} alt="Logo bytového družstva" />
-            <div className='flex flex-col w-full relative'>
-                <h1 className="text-center text-4xl font-bold pt-[40px] ">Bytové družstvo Porubská 552</h1>
-                <h3 className="text-center text-2xl pt-[20px]">Informační portál bytového družstva</h3>
+            <img className="w-[265px] mx-auto" src={logo} alt="Logo bytového družstva" />
+            <div className='flex flex-col w-full relative gap-3 md:gap-[20px]'>
+                <h1 className="text-center text-4xl font-bold md:mt-4">Bytové družstvo Porubská 552</h1>
+                <h3 className="text-center text-2xl">Informační portál bytového družstva</h3>
                 <Navbar />
             </div>
         </header>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu'
+import CloseIcon from '@mui/icons-material/Close';
 
 function Navbar() {
+    const [opened, setOpened] = useState(false)
+
+    const toggleMenu = () => {
+        setOpened(!opened)
+    }
+
     return (
-    <nav className='h-11 mt-auto w-full bg-sand-navbar'>
-        <ul className='flex flex-row h-full items-center justify-center gap-[15px]'>
+    <nav className='main-navbar h-full mt-auto w-full bg-sand-navbar md:h-11'>
+        <ul className='flex flex-row h-full items-center justify-center md:gap-[15px]'>
             <li className='nav-item'>
                 <Link to='/'>Domů</Link>
             </li>
@@ -25,7 +33,43 @@ function Navbar() {
             <li className='nav-item'>
                 <Link to='/contact'>Kontakt</Link>
             </li>
+            <div onClick={toggleMenu}>
+                {opened ?
+                    <CloseIcon className='hamburger-menu' style={{
+                        marginRight: "12px",
+                        display: "block",
+                        cursor: "pointer",
+                    }} />
+                    :
+                    <MenuIcon className='hamburger-menu' style={{
+                        marginRight: "12px",
+                        display: "block",
+                        cursor: "pointer",
+                    }} />
+                }
+            </div>
         </ul>    
+        {opened &&
+            <ul className='dropdown'>
+                <li className='nav-item-dropped'>
+                    <Link to='/about'>O nás</Link>
+                    {/* <ul>
+                        <li><Link to={'about/sprava'}>Správa domu</Link></li>
+                        <li><Link to={'about/predstavenstvo'}>Představenstvo</Link></li>
+                        <li className='nav-item'><Link to='about/history'>Historie</Link></li>
+                    </ul> */}
+                </li>
+                <li className='nav-item-dropped'>
+                    <Link to='/documents'>Dokumenty</Link>
+                </li>
+                <li className='nav-item-dropped'>
+                    <Link to='/fotogalery'>Fotogalerie</Link>
+                </li>
+                <li className='nav-item-dropped'>
+                    <Link to='/contact'>Kontakt</Link>
+                </li>
+            </ul>
+        }
     </nav>
     )    
 }
